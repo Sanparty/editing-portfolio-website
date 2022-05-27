c6ec2e8434b3745ef46a8e89a4b4dea3d1df1908
Sanparty <88841545+Sanparty@users.noreply.github.com>
2022-05-27 17:38:59 -0400

    linked home page to new javascript file
    new home.js javascript file has less elmeents... should load faster....
    
    removed other sections in full-container that were nodisplay

---
+++ js/home.js
@@ -0,0 +1,35 @@
+import videoObjectArray from "./components/data.js";
+
+let homeimagesall = videoObjectArray.filter(video => video.imagetype === "mp4");
+
+var homeimages = homeimagesall.filter((video,idx) => idx < 6)
+
+
+
+console.log("These are all the video objects with mp4s:", homeimagesall);
+console.log("These are first 6 video objects with mp4s", homeimages);
+
+const imageList = homeimages.map((video) => {
+  let videoArticle = document.createElement("div");
+  videoArticle.classList.add("front_image_container");
+
+  videoArticle.innerHTML = `
+  <a href="${video.pagelink}">   
+  <video class="front_image" autoplay loop muted width="480" height="270">
+  <source type="video/mp4" src="./${video.image}">
+  </video>
+  </a>
+    <div class="image_text"><a href="${video.pagelink}"><span>${video.genre}</span><hr class="new1"></a></div>
+  `;
+
+
+  return videoArticle;
+});
+
+
+
+const frontpage = document.querySelector(".image-block");
+
+imageList.forEach((video) => {
+  frontpage.append(video)
+});
