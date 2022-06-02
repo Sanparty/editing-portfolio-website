8cd93bbe6af80942e9427b04cabc231653cd64cf
Sanparty <88841545+Sanparty@users.noreply.github.com>
2022-06-01 22:34:15 -0400

    updated. video load

--- js/allspots.js
+++ js/allspots.js
@@ -29,21 +29,14 @@ let tmnVideos = videoObjectArray.filter(function (item) {
     return (item.brand == 'The Movie Network' || item.brand == 'The Movie Network Encore');  
 });
 
+let marketingVideos = videoObjectArray.filter(function (item) {
+    return (item.genre == "Social Media Marketing" || item.genre == "Corporate Marketing");  
+});
+
 
 
 console.log (tmnVideos);
 
-// let smarketingVideos = videoObjectArray.filter(function (item) {
-//     return item.genre == "Social Media Marketing";
-// });
-
-// let cmarketingVideos = videoObjectArray.filter(function (item) {
-//     return item.genre == "Corporate Marketing";
-// });
-
-// let featuresVideos = videoObjectArray.filter(function (item) {
-//     return item.genre == 'Features';
-// });
 
 
 
@@ -75,6 +68,10 @@ window.mlseShow = function mlseShow() {
 
 window.tmnShow = function tmnShow() {
     tmn()
+}
+
+window.marketingShow = function marketingShow() {
+    marketing()
 }
 // let promotionsButton = document.getElementById('promotionsBtn');
 // let featuresButton = document.getElementById('featuresBtn');
@@ -260,6 +257,40 @@ function tmn() {
     container.innerHTML = "";
    
     const promotionslist = tmnVideos.map((video) => {
+        let videoArticle = document.createElement("div");
+        videoArticle.classList.add("video");
+        videoArticle.setAttribute("id", video.id);
+
+        videoArticle.innerHTML = `
+      <figure class="video__spot">
+       ${video.videolocation}
+      </figure>
+      <h1 class="video__name">${video.promoName}</h1>
+      <ul class="video__details">
+        <li class="detail video__brand">Brand: <span>${video.brand}</span></li>
+        <li class="detail video__client">Client: <span>${video.client}</span></li>
+        <li class="detail video__role">Role: <span>${video.role}</span></li>
+        <li class="detail video__genre">Genre: <span>${video.genre}</span></li>
+        <li class="detail video__duration">Duration: <span>${video.duration}</span></li>
+      </ul>
+    `;
+        
+
+        return videoArticle;
+    });
+    // let brand = document.createElement("div");
+    // brand.innerHTML = `<h1>${tmnVideos.brand}</h1>`;
+    // container.insertBefore(brand);
+
+    promotionslist.forEach((video) => {
+        container.append(video)
+    });
+}
+
+function marketing() {
+    container.innerHTML = "";
+   
+    const promotionslist = marketingVideos.map((video) => {
         let videoArticle = document.createElement("div");
         videoArticle.classList.add("video");
         videoArticle.setAttribute("id", video.id);
