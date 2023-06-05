d8ff2f705fc60892bcf38819dad6ed30be0681a8
Sanparty <88841545+Sanparty@users.noreply.github.com>
2023-06-05 16:23:36 -0400

    added spiderman spot

--- js/components/data.js
+++ js/components/data.js
@@ -533,8 +533,8 @@ const gb2 = new Video(
             `<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/824110372?h=289482483f&title=0&byline=0&portrait=0&speed=0&badge=0&autopause=0&player_id=0&app_id=58479/embed" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
             `,
             "May 5, 2023",
-            "images/THE TERMINATOR STARZ PROMO-low.mp4",
-            "mp4",
+            "none",
+            "none",
             "promotions.html"
           
           );
