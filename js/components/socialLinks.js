f49fd8db1f89db0b6da4a4a750889989387205b8
Sanparty <dansp@me.com>
2025-02-04 13:51:27 -0500

    social links data

---
+++ js/components/socialLinks.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const githubLink = {
+  name: "Github",
+  id: "github",
+  image: "291716_github_logo_social network_social_icon.svg",
+  alt: "GitHub Logo",
+  link: "https://github.com/Sanparty",
+};
+const linkedinLink = {
+  name: "LinkedIn",
+  id: "linkedin",
+  image: "194920_linkedin_social media_icon.svg",
+  alt: "LinkedIn Logo",
+  link: "https://www.linkedin.com/in/dsanpedro/",
+};
+const vimeoLink = {
+  name: "Vimeo",
+  id: "vimeo",
+  image: "2083189_media_movie_share_social_video_icon.svg",
+  alt: "Vimeo Logo",
+  link: "https://vimeo.com/dansp",
+};
+const blueskyLink = {
+  name: "BlueSky",
+  id: "bluesky",
+  image: "bluesky-black-round-circle-outline-logo-24465.svg",
+  alt: "BlueSky Logo",
+  link: "https://bsky.app/profile/sanparty.bsky.social",
+};
+const socialLinks = [githubLink, linkedinLink, vimeoLink, blueskyLink];
+
+export default socialLinks;
