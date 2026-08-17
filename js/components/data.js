cf94a6a5a8b19664f7b088b30fb00ef88c51fbce
Sanparty <dansp@me.com>
2026-08-17 09:05:19 -0400

    changed duration of tmn on demand spot

--- js/components/data.js
+++ js/components/data.js
@@ -849,7 +849,7 @@ const motionDesign3 = new Video(
   true,
   false,
   false,
-  ":45",
+  "1:00",
   "Vimeo",
   `<div style="padding:57.83% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/1218748277?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" referrerpolicy="strict-origin-when-cross-origin" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="TMN On Demand Spot"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>`,
   "August 16, 2025",
